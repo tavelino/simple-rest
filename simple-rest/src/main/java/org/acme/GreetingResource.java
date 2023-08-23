e6898e2fc5cf8205d2c4b2daf434d29bafaf6c46
Tiago Avelino <tavelino@redhat.com>
2023-08-23 10:48:44 -0300

    Ajuste repo

--- simple-rest/src/main/java/org/acme/GreetingResource.java
+++ simple-rest/src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Simple test UCF";
+        return "Hello from RESTEasy Reactive";
     }
 }
