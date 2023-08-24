fdd87afbd78965299a202078280396a2588e6362
Tiago Avelino <tavelino@redhat.com>
2023-08-24 09:49:46 -0300

    Mudanca de mensagem

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello BB UCF !!\n");
+  res.send("Hello BB \n");
 });
 
 app.listen(8080, function () {
